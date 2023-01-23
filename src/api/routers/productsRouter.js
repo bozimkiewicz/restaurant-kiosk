976e03fffabcdfaef63c38e46dd5dc2718296c25
bozimkiewicz <bartek.ozim@wp.pl>
2023-01-23 19:51:36 +0100

    Added GET all categories and products by category

--- src/api/routers/productsRouter.js
+++ src/api/routers/productsRouter.js
@@ -16,4 +16,32 @@ productsRouter.get("/all", (req, res) => {
     .catch((error) => console.log(error));
 });
 
+productsRouter.get("/categories", (req, res) => {
+  const session = driver.session();
+  session
+    .run("MATCH (n:Categories) RETURN n")
+    .then((data) => {
+      const categories = data.records.map(
+        (category) => category._fields[0].properties
+      );
+      res.send(categories);
+    })
+    .catch((error) => console.log(error));
+});
+
+productsRouter.get("/:category", (req, res) => {
+  const session = driver.session();
+  session
+    .run(
+      `MATCH (n:Products)-[:has_category]->(m:Categories WHERE m.name = '${req.params.category}') RETURN n`
+    )
+    .then((data) => {
+      const products = data.records.map(
+        (product) => product._fields[0].properties
+      );
+      res.send(products);
+    })
+    .catch((error) => console.log(error));
+});
+
 module.exports = productsRouter;
