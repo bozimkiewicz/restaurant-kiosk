4c88b512a05f374409a0cfbc999c934b8d4620fc
bozimkiewicz <bartek.ozim@wp.pl>
2023-01-24 17:43:56 +0100

    Added GET request for ingredients in product

--- src/api/routers/productsRouter.js
+++ src/api/routers/productsRouter.js
@@ -61,4 +61,36 @@ productsRouter.get("/ingredients/all", (req, res) => {
     .then(() => session.close());
 });
 
+productsRouter.get("/:product/ingredients", (req, res) => {
+  const session = driver.session();
+
+  session
+    .run(
+      `MATCH (n:Products)-[:has_ingredient]->(m:Ingredients) WHERE n.id=${req.params.product} RETURN m`
+    )
+    .then((data) =>
+      data.records.map((ingredient) => ingredient._fields[0].properties)
+    )
+    .then((product_ingredients) => {
+      return session
+        .run(`MATCH (i:Ingredients) RETURN i`)
+        .then((data) => data.records.map((i) => i._fields[0].properties))
+        .then((all_ingredients) => {
+          return {
+            all_ingredients: all_ingredients,
+            product_ingredients: product_ingredients,
+          };
+        });
+    })
+    .then((data) => {
+      session.close();
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ message: "Internal server Error" });
+    });
+});
+
+
 module.exports = productsRouter;
