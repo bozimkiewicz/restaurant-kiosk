813780b217694073b932e8547585546ee7d0384d
bozimkiewicz <bartek.ozim@wp.pl>
2023-01-27 21:01:36 +0100

    changed styling in order method

--- src/components/OrderMethod.tsx
+++ src/components/OrderMethod.tsx
@@ -1,11 +1,10 @@
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom'
-import { setTakeIn, setTakeaway } from '../slices/OrderSlice';
-
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { setTakeIn, setTakeaway } from "../slices/OrderSlice";
 
 const OrderMethod = () => {
-  const dispatch = useDispatch()
-  
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>Wybierz sposób składania zamówienia!</h1>
@@ -13,11 +12,13 @@ const OrderMethod = () => {
         <button onClick={() => dispatch(setTakeaway())}>Na wynos</button>
       </Link>
       <Link to={"/home"}>
-        <button onClick={() => dispatch(setTakeIn())}>Na miejscu</button>
+        <button className="ml-2" onClick={() => dispatch(setTakeIn())}>
+          Na miejscu
+        </button>
       </Link>
-      <div>
+      <div className="flex justify-start">
         <Link to="/">
-          <button>Cofnij</button>
+          <button className="mt-5">Cofnij</button>
         </Link>
       </div>
     </div>
