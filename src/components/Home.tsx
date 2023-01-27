f3a20530c39c87533b758c9a91530de58784cf78
bozimkiewicz <bartek.ozim@wp.pl>
2023-01-27 21:02:26 +0100

    added admin route links in home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,11 +5,19 @@ import Products from "./Products/Products";
 const Home = () => {
   return (
     <div>
+      <div className="flex justify-end">
+        <Link to="/crud">
+          <button>Admin</button>
+        </Link>
+        <Link to="/home">
+          <button className="ml-2">Home</button>
+        </Link>
+      </div>
       <div className="flex justify-between p-5 mt-5 bg-white rounded-md shadow-md">
         <Products />
         <Cart />
       </div>
-      <div className="flex justify-start p-5">
+      <div className="flex justify-start mt-5">
         <Link to="/order-method">
           <button>Cofnij</button>
         </Link>
