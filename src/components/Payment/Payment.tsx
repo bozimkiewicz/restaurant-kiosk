316dcc399630507e2af4b4bdd40edb4979177fb3
bozimkiewicz <bartek.ozim@wp.pl>
2023-01-28 09:57:59 +0100

    fixed bug (cart wasn't empty after payment)

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -1,8 +1,11 @@
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { removeItems } from "../../slices/CartSlice";
 import ICartProducts from "../interfaces/ICartProducts";
 
 const Payment = () => {
+  const dispatch = useDispatch();
 
   const orderMethod = useSelector<
     { order: { method: string } },
@@ -19,25 +22,28 @@ const Payment = () => {
     0
   );
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const newOrder = (payment: string) => {
-    fetch('http://localhost:3000/orders', {
-      method: 'POST',
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         payment: payment,
         order_method: orderMethod.method,
         items: JSON.stringify(cartContent),
-        total: cartItemsTotal
-      })
+        total: cartItemsTotal,
+      }),
     })
-      .then(response => response.json())
-      .then(() => navigate('/'))
-      .catch(err => console.log(err))
-  }
+      .then((response) => response.json())
+      .then(() => {
+        dispatch(removeItems());
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
 
   const listedCartContent = (
     <ul>
@@ -61,8 +67,12 @@ const Payment = () => {
         <h2>Zamówienie {orderMethod.method}</h2>
         {listedCartContent}
         <h2>Kwota do zapłaty: {cartItemsTotal.toFixed(2)} zł</h2>
-        <button className="mr-3" onClick={() => newOrder('card')}>Płatność kartą tutaj</button>
-        <button onClick={() => newOrder('cash')}>Płatność gotówką przy kasie</button>
+        <button className="mr-3" onClick={() => newOrder("card")}>
+          Płatność kartą tutaj
+        </button>
+        <button onClick={() => newOrder("cash")}>
+          Płatność gotówką przy kasie
+        </button>
       </div>
       <div className="flex justify-start mt-5">
         <Link to="/home">
